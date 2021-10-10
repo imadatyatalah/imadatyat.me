a048d2c4c576353bce20969dba0059095318b1d2
imad <imadatyatalah@gmail.com>
2021-10-10 18:11:05 +0100

    feat: Added custom 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Custom404 = () => (
+  <section className="flex flex-col items-center justify-center min-h-screen">
+    <div>
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+    </div>
+  </section>
+);
+
+export default Custom404;
