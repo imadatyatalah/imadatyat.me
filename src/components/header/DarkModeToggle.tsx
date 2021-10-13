530d95709efd2a896d02efc5954fc6fb609594a4
imad <imadatyatalah@gmail.com>
2021-10-13 20:37:58 +0100

    refactor: Improve dark mode toggle

--- src/components/header/DarkModeToggle.tsx
+++ src/components/header/DarkModeToggle.tsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import { useTheme } from "next-themes";
 
-import SunIcon from "@/icons/Sun";
-import MoonIcon from "@/icons/Moon";
-
 const DarkModeToggle = () => {
   const { resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
@@ -13,23 +10,18 @@ const DarkModeToggle = () => {
 
   if (!mounted) return null;
 
-  const toggleTheme = () =>
-    setTheme(resolvedTheme === "light" ? "dark" : "light");
+  const isLightMode = resolvedTheme === "light";
+
+  const toggleTheme = () => setTheme(isLightMode ? "dark" : "light");
 
   return (
-    <div>
-      <button
-        aria-label="Toggle Dark Mode"
-        type="button"
-        className="p-2 font-semibold transition-all bg-gray-200 rounded-md dark:bg-gray-600 hover:ring-2 ring-gray-300"
-        onClick={toggleTheme}
-      >
-        {resolvedTheme === "light" ? (
-          <MoonIcon className="w-5 h-5" />
-        ) : (
-          <SunIcon className="w-5 h-5" />
-        )}
-      </button>
+    <div
+      className={`toggle-track ${isLightMode ? "toggled" : null}`}
+      onClick={toggleTheme}
+    >
+      <div className="toggle-moon">🌛</div>
+      <div className="toggle-sun">☀️</div>
+      <div className="toggle-thumb"></div>
     </div>
   );
 };
