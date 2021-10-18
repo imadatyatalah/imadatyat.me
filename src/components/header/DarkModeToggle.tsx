e60dd1be7ec696090071c13a34c981b75e5abf04
imad <imadatyatalah@gmail.com>
2021-10-18 14:01:53 +0100

    refactor: Change dark mode toggle

--- src/components/header/DarkModeToggle.tsx
+++ src/components/header/DarkModeToggle.tsx
@@ -14,15 +14,43 @@ const DarkModeToggle = () => {
 
   const toggleTheme = () => setTheme(isLightMode ? "dark" : "light");
 
+  const buttonAriaLabel = isLightMode
+    ? "Toggle Dark Mode"
+    : "Toggle Light Mode";
+
   return (
-    <div
-      className={`toggle-track ${isLightMode ? "toggled" : null}`}
+    <button
+      aria-label={buttonAriaLabel}
+      type="button"
+      className="flex items-center justify-center transition-all bg-gray-200 rounded-lg w-9 h-9 dark:bg-gray-600 hover:ring-2 ring-gray-300"
       onClick={toggleTheme}
     >
-      <div className="toggle-moon">🌛</div>
-      <div className="toggle-sun">☀️</div>
-      <div className="toggle-thumb"></div>
-    </div>
+      {mounted && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          className="w-5 h-5 text-gray-800 dark:text-gray-200"
+        >
+          {resolvedTheme === "dark" ? (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          ) : (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          )}
+        </svg>
+      )}
+    </button>
   );
 };
 
