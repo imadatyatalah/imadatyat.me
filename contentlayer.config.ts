653e27d60b00ea7dcf3d76eadd9b91822ac1fc40
imad <imadatyatalah@gmail.com>
2021-10-20 10:47:38 +0100

    feat: Added Guide fields

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -43,7 +43,12 @@ const Guides = defineDocumentType(() => ({
   name: "Guides",
   filePathPattern: "guides/*.mdx",
   bodyType: "mdx",
-  fields: {},
+  fields: {
+    title: { type: "string", required: true },
+    publishedAt: { type: "string", required: true },
+    description: { type: "string", required: true },
+    image: { type: "string", required: false },
+  },
   computedFields,
 }));
 
